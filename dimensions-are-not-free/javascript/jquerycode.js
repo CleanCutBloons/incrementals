f5d14f13d0a20cfd295da438dd2e9154e61febd9
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2021-03-04 13:45:34 -0500

    Update jquerycode.js

--- dimensions-are-not-free/javascript/jquerycode.js
+++ dimensions-are-not-free/javascript/jquerycode.js
@@ -5,8 +5,8 @@ var jQuery = function(player){
   $("span#nd2Amount").text(player.normalDimensions[1][2]);
   $("span#nd3Amount").text(player.normalDimensions[2][2]);
   $("span#nd4Amount").text(player.normalDimensions[3][2]);
-  $("span#nd1Cost").text(player.normalDimensions[0][1].toString());
-  $("span#nd2Cost").text(player.normalDimensions[1][1].toString());
-  $("span#nd3Cost").text(player.normalDimensions[2][1].toString());
-  $("span#nd4Cost").text(player.normalDimensions[3][1].toString());
+  $("span#nd1Cost").text(displayExp(player.normalDimensions[0][1]));
+  $("span#nd2Cost").text(displayExp(player.normalDimensions[1][1]));
+  $("span#nd3Cost").text(displayExp(player.normalDimensions[2][1]));
+  $("span#nd4Cost").text(displayExp(player.normalDimensions[3][1]));
 };
