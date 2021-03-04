7d24e40272312879168bcdb75f66b15c6d3a61a5
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2021-03-04 12:16:21 -0500

    Add code for ND1-4 amounts

--- dimensions-are-not-free/javascript/jquerycode.js
+++ dimensions-are-not-free/javascript/jquerycode.js
@@ -1,3 +1,8 @@
 var jQuery = function(){
-  $("span.points").text(Math.round(points));
+  Decimal.set({ rounding: 7 })
+  $("span.points").text(points.round());
+  $("span#nd1Amount").text(normalDimensions[0][2]);
+  $("span#nd2Amount").text(normalDimensions[1][2]);
+  $("span#nd3Amount").text(normalDimensions[2][2]);
+  $("span#nd4Amount").text(normalDimensions[3][2]);
 };
