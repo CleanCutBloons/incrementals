a8bbda9dc3490f87e3c564c8add8256428007f2f
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2021-03-04 12:44:36 -0500

    Update gameLoop.js

--- dimensions-are-not-free/javascript/gameLoop.js
+++ dimensions-are-not-free/javascript/gameLoop.js
@@ -5,7 +5,7 @@ var draw = function(){
   var points = player.points;
   var normalDimensions = player.normalDimensions;
   points += 0.05;
-  jQuery();
   player.points = points;
   player.normalDimensions = player.normalDimensions;
+  jQuery();
 };
