d1a50e2abb0deb480dccaa021b5cd98181bc66c1
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2021-03-04 12:19:34 -0500

    Update jquerycode.js

--- dimensions-are-not-free/javascript/jquerycode.js
+++ dimensions-are-not-free/javascript/jquerycode.js
@@ -1,8 +1,12 @@
 var jQuery = function(){
-  Decimal.set({ rounding: 7 })
-  $("span.points").text(points.round());
+  Decimal.set({ toExpPos: 6 })
+  $("span.points").text(points.toString());
   $("span#nd1Amount").text(normalDimensions[0][2]);
   $("span#nd2Amount").text(normalDimensions[1][2]);
   $("span#nd3Amount").text(normalDimensions[2][2]);
   $("span#nd4Amount").text(normalDimensions[3][2]);
+  $("span#nd1Cost").text(normalDimensions[0][1].toString());
+  $("span#nd2Cost").text(normalDimensions[1][1].toString());
+  $("span#nd3Cost").text(normalDimensions[2][1].toString());
+  $("span#nd4Cost").text(normalDimensions[3][1].toString());
 };
