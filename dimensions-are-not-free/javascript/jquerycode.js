f088eb11c677eb083b84e2a394e5d33257af619e
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2021-03-04 12:44:17 -0500

    Update jquerycode.js

--- dimensions-are-not-free/javascript/jquerycode.js
+++ dimensions-are-not-free/javascript/jquerycode.js
@@ -1,12 +1,12 @@
-var jQuery = function(){
+var jQuery = function(player){
   Decimal.toExpPos = 6;
-  $("span.points").text(points.toString());
-  $("span#nd1Amount").text(normalDimensions[0][2]);
-  $("span#nd2Amount").text(normalDimensions[1][2]);
-  $("span#nd3Amount").text(normalDimensions[2][2]);
-  $("span#nd4Amount").text(normalDimensions[3][2]);
-  $("span#nd1Cost").text(normalDimensions[0][1].toString());
-  $("span#nd2Cost").text(normalDimensions[1][1].toString());
-  $("span#nd3Cost").text(normalDimensions[2][1].toString());
-  $("span#nd4Cost").text(normalDimensions[3][1].toString());
+  $("span.points").text(player.points.toString());
+  $("span#nd1Amount").text(player.normalDimensions[0][2]);
+  $("span#nd2Amount").text(player.normalDimensions[1][2]);
+  $("span#nd3Amount").text(player.normalDimensions[2][2]);
+  $("span#nd4Amount").text(player.normalDimensions[3][2]);
+  $("span#nd1Cost").text(player.normalDimensions[0][1].toString());
+  $("span#nd2Cost").text(player.normalDimensions[1][1].toString());
+  $("span#nd3Cost").text(player.normalDimensions[2][1].toString());
+  $("span#nd4Cost").text(player.normalDimensions[3][1].toString());
 };
