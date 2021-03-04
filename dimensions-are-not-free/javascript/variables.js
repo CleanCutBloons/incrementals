26a781cca837e3cffcc9d4ce01b5df10e2bd4727
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2021-03-04 12:02:24 -0500

    Update variables.js

--- dimensions-are-not-free/javascript/variables.js
+++ dimensions-are-not-free/javascript/variables.js
@@ -1,2 +1,4 @@
-var points = new Decimal(1);
-var normalDimensions = [[0, new Decimal(1), 0],[1, new Decimal(100), 0],[2, new Decimal("1e5"), 0],[3, new Decimal("1e9")]]
+var player = {
+  points: new Decimal(1),
+  normalDimensions: [[0, new Decimal(1), 0],[1, new Decimal(100), 0],[2, new Decimal("1e5"), 0],[3, new Decimal("1e9")]],
+};
