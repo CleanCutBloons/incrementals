c0383a4e31d7060e8b4647d766a25c42c1c58cb6
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2021-03-05 14:22:21 -0500

    Update purchase.js

--- dimensions-are-not-free/javascript/purchase.js
+++ dimensions-are-not-free/javascript/purchase.js
@@ -5,6 +5,7 @@ var purchase = function(dimType, dimNum){
     if(pts.gte(cost)){
       pts = pts.sub(cost);
       cost = cost.mul(Decimal.pow(5, dimNum));
+      player.normalDimensions[dimNum-1][2].add(1);
       player.points = pts;
       player.normalDimensions[dimNum-1][1] = cost;
     }
